9e11075efbc296bd70e64d8683e681ae1c1c7fc0
iryd97 <waszpaniwladca@op.pl>
2015-09-13 16:26:23 +0200

    Enemy init3

--- core/src/com/dobromir/rebellion/map/objects/Enemy.java
+++ core/src/com/dobromir/rebellion/map/objects/Enemy.java
@@ -1,10 +1,18 @@
 package com.dobromir.rebellion.map.objects;
 
+import com.dobromir.rebellion.Game;
+import com.dobromir.rebellion.sprites.GameSprite;
+
 /**
  * Created by Micha³ on 2015-09-13.
  */
-public class Enemy {
-    public Enemy() {
+public class Enemy extends GameSprite {
 
+    public Enemy(Game game, float x, float y) {
+        super(game, x, y);
+    }
+
+    public Enemy(String skinName, Game game, float x, float y) {
+        super(skinName, game, x, y);
     }
 }
