18bda60b145dd3c4bbd446388f94d495d0470ce0
iryd97 <waszpaniwladca@op.pl>
2015-09-14 19:46:56 +0200

    Enemy init4

--- core/src/com/dobromir/rebellion/map/objects/Enemy.java
+++ core/src/com/dobromir/rebellion/map/objects/Enemy.java
@@ -1,6 +1,8 @@
 package com.dobromir.rebellion.map.objects;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.dobromir.rebellion.Game;
+import com.dobromir.rebellion.data.ImageCache;
 import com.dobromir.rebellion.sprites.GameSprite;
 
 /**
@@ -8,8 +10,21 @@ import com.dobromir.rebellion.sprites.GameSprite;
  */
 public class Enemy extends GameSprite {
 
+    private boolean death = false;
+
+    private Sprite sprite;
+
     public Enemy(Game game, float x, float y) {
-        super(game, x, y);
+        super(game, x, y, 100);//TODO tu jest error ale nie wiem czemu
+        texture = null;
+
+        sprite = new Sprite(ImageCache.getTexture("enemy"));
+        width = sprite.getWidth();
+        height = sprite.getHeight();
+
+
     }
+
+
 //TODO:zrobic przeciwnika
 }
