1799f9538d5bfeb5529ed97914c6f8f7d2cbfe9d
KosuDS <daniel.karol.skorupski@gmail.com>
2015-09-15 22:38:29 +0200

    Polygony sa zamkniete i nie powtarza sie pierwszego x, y

--- core/src/com/dobromir/rebellion/sprites/MovingSprite.java
+++ core/src/com/dobromir/rebellion/sprites/MovingSprite.java
@@ -98,6 +98,8 @@ public class MovingSprite extends GameSprite {
         if(collisionY) {
             directionMove[1] = 0;
         }
+
+//        TODO: Okazalo sie ze polygony sa zamkniete wiec nie trzeba powtarzac pierwszego x, y
     }
 
     private boolean isCellBlocked(float x, float y) {
