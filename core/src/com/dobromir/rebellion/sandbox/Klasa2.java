55d81738d268a0bb5302c0195919e3bd8fc56529
iryd97 <waszpaniwladca@op.pl>
2015-09-18 17:04:40 +0200

    klasa2 init

--- core/src/com/dobromir/rebellion/sandbox/Klasa2.java
+++ core/src/com/dobromir/rebellion/sandbox/Klasa2.java
@@ -3,6 +3,6 @@ package com.dobromir.rebellion.sandbox;
 public class Klasa2 {
     public static void main(String[] args) {
         Klasy klasa = new Klasy(20, 10);
-        System.out.println(klasa.suma());
+        System.out.println(klasa.suma() + " hello world");
     }
 }
